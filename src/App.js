cad0c009c648b412389020a5b616f6d1a9a2fdd7
Aayush Bharti <aayushbh2002@gmail.com>
2024-02-29 00:28:47 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,148 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import './parallax.css';
+import logo from './assets/aayush.webm';
 
 function App() {
   return (
     <div className="">
+
+      {/* Parallax */}
+      <section className="animated-background">
+        <div id="object1"></div>
+        <div id="object2"></div>
+        <div id="object3"></div>
+      </section>
       
-    </div>
+      <section className="relative w-full overflow-x-hidden">
+        <div className="absolute w-0 h-0 hidden"><img src="./assets/Kingdom.jpeg" alt="" /></div>  {/*just for loading*/}
+        <div className="relative w-max-[1080px] mt-12 mx-auto flex flex-col justify-center items-center">
+
+          <button onclick="changeIcon()" id="themeButton"
+            className="theme-button w-[2em] h-[2em] p-1 absolute right-5 top-0 cursor-pointer flex 
+              justify-start items-center
+              rounded-full hover:bg-[#ffffff19] transition-all duration-200">
+            <svg className="light-mode-svg transition-all duration-500"     viewBox="0 0 24 24"><g fill="#ffff"><circle r="5" cy="12" cx="12"></circle><path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path></g></svg>
+
+            <svg className="dark-mode-svg w-5  transition-all duration-500" fill="#fff"  viewBox="0 0 384 512"><path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg>
+          </button>
+
+          <div className="flex flex-col justify-center items-center">
+            <a href="#">
+                {/*<img class="h-[8em] rounded-full logo" src="/assets/small c.gif" alt="">*/}
+              <video loop autoplay muted class="h-[7.5em] rounded-full">
+                <source src={logo} type="video/webm"/>
+              </video>
+            </a>
+
+            <h2 className="text-center font-bold text-white text-2xl drop-shadow-md mt-5">Aayush Bharti</h2>
+
+            <a className="text-center text-slate-100 mt-2 text-lg drop-shadow-md"
+              href="mailto:iaayushbharti@gmail.com?subject=Hello Aayush" target="_blank" rel='noreferrer'>
+              iaayushbharti@gmail.com
+            </a>
+          </div>
+
+
+          <div className="flex justify-center items-center mt-2 p-1 px-6 gap-5 rounded-full hover:backdrop-blur
+          hover:bg-[#ffffff28] bg-[#ffffff17]">
+
+            <a href="https://t.me/notesLib" target="_blank" rel='noreferrer'>
+              <svg className="hover:fill-[#00a2f9] fill-white bar-icon" viewBox="0 0 100 100" version="1.1" height="20" width="20">
+                <path
+                  d="M95,9.9c-1.3-1.1-3.4-1.2-7-0.1c0,0,0,0,0,0c-2.5,0.8-24.7,9.2-44.3,17.3c-17.6,7.3-31.9,13.7-33.6,14.5  c-1.9,0.6-6,2.4-6.2,5.2c-0.1,1.8,1.4,3.4,4.3,4.7c3.1,1.6,16.8,6.2,19.7,7.1c1,3.4,6.9,23.3,7.2,24.5c0.4,1.8,1.6,2.8,2.2,3.2  c0.1,0.1,0.3,0.3,0.5,0.4c0.3,0.2,0.7,0.3,1.2,0.3c0.7,0,1.5-0.3,2.2-0.8c3.7-3,10.1-9.7,11.9-11.6c7.9,6.2,16.5,13.1,17.3,13.9  c0,0,0.1,0.1,0.1,0.1c1.9,1.6,3.9,2.5,5.7,2.5c0.6,0,1.2-0.1,1.8-0.3c2.1-0.7,3.6-2.7,4.1-5.4c0-0.1,0.1-0.5,0.3-1.2  c3.4-14.8,6.1-27.8,8.3-38.7c2.1-10.7,3.8-21.2,4.8-26.8c0.2-1.4,0.4-2.5,0.5-3.2C96.3,13.5,96.5,11.2,95,9.9z M30,58.3l47.7-31.6  c0.1-0.1,0.3-0.2,0.4-0.3c0,0,0,0,0,0c0.1,0,0.1-0.1,0.2-0.1c0.1,0,0.1,0,0.2-0.1c-0.1,0.1-0.2,0.4-0.4,0.6L66,38.1  c-8.4,7.7-19.4,17.8-26.7,24.4c0,0,0,0,0,0.1c0,0-0.1,0.1-0.1,0.1c0,0,0,0.1-0.1,0.1c0,0.1,0,0.1-0.1,0.2c0,0,0,0.1,0,0.1  c0,0,0,0,0,0.1c-0.5,5.6-1.4,15.2-1.8,19.5c0,0,0,0,0-0.1C36.8,81.4,31.2,62.3,30,58.3z"
+                ></path>
+              </svg>
+            </a>
+
+            <a href="mailto:iaayushbharti@gmail.com?subject=Hello Aayush" target="_blank" rel='noreferrer'>
+              {/* <svg className="hover:fill-[#c50629] fill-white bar-icon"     height="20" width="20"      height="800px" width="800px" version="1.1" id="_x32_" viewBox="0 0 512 512" xml:space="preserve">
+                            <g>
+                                <path className="st0" d="M510.678,112.275c-2.308-11.626-7.463-22.265-14.662-31.054c-1.518-1.915-3.104-3.63-4.823-5.345   c-12.755-12.818-30.657-20.814-50.214-20.814H71.021c-19.557,0-37.395,7.996-50.21,20.814c-1.715,1.715-3.301,3.43-4.823,5.345   C8.785,90.009,3.63,100.649,1.386,112.275C0.464,116.762,0,121.399,0,126.087V385.92c0,9.968,2.114,19.55,5.884,28.203   c3.497,8.26,8.653,15.734,14.926,22.001c1.59,1.586,3.169,3.044,4.892,4.494c12.286,10.175,28.145,16.32,45.319,16.32h369.958   c17.18,0,33.108-6.145,45.323-16.384c1.718-1.386,3.305-2.844,4.891-4.43c6.27-6.267,11.425-13.741,14.994-22.001v-0.064   c3.769-8.653,5.812-18.171,5.812-28.138V126.087C512,121.399,511.543,116.762,510.678,112.275z M46.509,101.571   c6.345-6.338,14.866-10.175,24.512-10.175h369.958c9.646,0,18.242,3.837,24.512,10.175c1.122,1.129,2.179,2.387,3.112,3.637   L274.696,274.203c-5.348,4.687-11.954,7.002-18.696,7.002c-6.674,0-13.276-2.315-18.695-7.002L43.472,105.136   C44.33,103.886,45.387,102.7,46.509,101.571z M36.334,385.92V142.735L176.658,265.15L36.405,387.435   C36.334,386.971,36.334,386.449,36.334,385.92z M440.979,420.597H71.021c-6.281,0-12.158-1.651-17.174-4.552l147.978-128.959   l13.815,12.018c11.561,10.046,26.028,15.134,40.36,15.134c14.406,0,28.872-5.088,40.432-15.134l13.808-12.018l147.92,128.959   C453.137,418.946,447.26,420.597,440.979,420.597z M475.666,385.92c0,0.529,0,1.051-0.068,1.515L335.346,265.221L475.666,142.8   V385.92z"/>
+                            </g>
+                        </svg> */}
+            </a>
+
+            <a href="https://www.linkedin.com/in/iAayushBharti" target="_blank" rel='noreferrer'>
+              <svg className="hover:fill-[#0061d7] fill-white bar-icon"     height="20" width="20" viewBox="0 0 448 512" fill="#fff"><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path></svg>
+            </a>
+          </div>
+
+
+
+
+
+
+
+          <div className="flex flex-col mt-8 justify-center items-center gap-5 px-1 py-4">
+
+            <a className="button portfolio-btn"
+              href="https://aayushbharti.vercel.app" target="_blank" rel='noreferrer'>
+              <svg className="mr-4" height="1em"          viewBox="0 0 20 20" version="1.1">
+                <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#ffff">
+                  <g id="icons" transform="translate(56.000000, 160.000000)">
+                    <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]"></path>
+                  </g>
+                </g>
+              </svg>
+              Personal Portfolio Website
+            </a>
+
+            <a className="button telegram"
+              href="https://t.me/notesLib" target="_blank" rel='noreferrer'>
+              <img className=" rounded-full h-[1.2em] mr-4"
+                src="../assets/icons8-telegram-app.gif" alt=""/>
+              Telegram (FREE N🎯TES)
+            </a>
+
+            <a className="button github"
+              href="https://github.com/AayushBharti" target="_blank" rel='noreferrer'>
+              <svg className="mr-4" height="1em" viewBox="0 0 20 20" version="1.1">
+                <g id="Dribbble-Light-Preview" transform="translate(-140.000000, -7559.000000)" fill="#ffff">
+                  <g id="icons" transform="translate(56.000000, 160.000000)">
+                    <path d="M94,7399 C99.523,7399 104,7403.59 104,7409.253 C104,7413.782 101.138,7417.624 97.167,7418.981 C96.66,7419.082 96.48,7418.762 96.48,7418.489 C96.48,7418.151 96.492,7417.047 96.492,7415.675 C96.492,7414.719 96.172,7414.095 95.813,7413.777 C98.04,7413.523 100.38,7412.656 100.38,7408.718 C100.38,7407.598 99.992,7406.684 99.35,7405.966 C99.454,7405.707 99.797,7404.664 99.252,7403.252 C99.252,7403.252 98.414,7402.977 96.505,7404.303 C95.706,7404.076 94.85,7403.962 94,7403.958 C93.15,7403.962 92.295,7404.076 91.497,7404.303 C89.586,7402.977 88.746,7403.252 88.746,7403.252 C88.203,7404.664 88.546,7405.707 88.649,7405.966 C88.01,7406.684 87.619,7407.598 87.619,7408.718 C87.619,7412.646 89.954,7413.526 92.175,7413.785 C91.889,7414.041 91.63,7414.493 91.54,7415.156 C90.97,7415.418 89.522,7415.871 88.63,7414.304 C88.63,7414.304 88.101,7413.319 87.097,7413.247 C87.097,7413.247 86.122,7413.234 87.029,7413.87 C87.029,7413.87 87.684,7414.185 88.139,7415.37 C88.139,7415.37 88.726,7417.2 91.508,7416.58 C91.513,7417.437 91.522,7418.245 91.522,7418.489 C91.522,7418.76 91.338,7419.077 90.839,7418.982 C86.865,7417.627 84,7413.783 84,7409.253 C84,7403.59 88.478,7399 94,7399" id="github-[#142]"></path>
+                  </g>
+                </g>
+              </svg>
+              Github
+            </a>
+
+            <a className="button linkedin"
+              href="https://www.linkedin.com/in/iAayushBharti" target="_blank" rel='noreferrer'>
+              <img className=" rounded-full h-[1.2em] mr-4"
+                src="../assets/GNvM541FBQu7.gif" alt="" />
+              Linkedin
+            </a>
+
+            <a className="button x"
+              href="https://twitter.com/iAayushBharti" target="_blank" rel='noreferrer'>
+              <svg className="mr-4"
+                viewBox="0 0 512 512"
+                height="1em"
+                  
+                // className="svgIcon"
+                fill="white"
+              >
+                <path
+                  d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"
+                ></path>
+              </svg>
+              Elon's X
+            </a>
+
+            <a className="button message"
+              href="https://aayushbharti.sayout.net" target="_blank" rel='noreferrer'>
+              <svg className="mr-4 mt-1"     height="1.1em" fill="none" viewBox="0 0 24 24" >
+                <path d="M7 9H17M7 13H12M21 20L17.6757 18.3378C17.4237 18.2118 17.2977 18.1488 17.1656 18.1044C17.0484 18.065 16.9277 18.0365 16.8052 18.0193C16.6672 18 16.5263 18 16.2446 18H6.2C5.07989 18 4.51984 18 4.09202 17.782C3.71569 17.5903 3.40973 17.2843 3.21799 16.908C3 16.4802 3 15.9201 3 14.8V7.2C3 6.07989 3 5.51984 3.21799 5.09202C3.40973 4.71569 3.71569 4.40973 4.09202 4.21799C4.51984 4 5.0799 4 6.2 4H17.8C18.9201 4 19.4802 4 19.908 4.21799C20.2843 4.40973 20.5903 4.71569 20.782 5.09202C21 5.51984 21 6.0799 21 7.2V20Z" stroke="#ffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+              Anonymous Message
+            </a>
+            
+          </div>
+        </div>
+      </section >
+
+    </div >
   );
 }
 
