a72b74027eacf66d9c99f153ae6552ba0b4563a8
Aayush Bharti <aayushbh2002@gmail.com>
2024-02-29 17:00:22 +0530

    bug fixed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,15 @@ import './parallax.css';
 import logo from './assets/aayush.webm';
 
 function App() {
+
+  function changeIcon()
+  {   
+      // document.body.style.backgroundImage="url(./assets/Kingdom.jpeg)";
+      document.body.classList.toggle("dark-mode");
+      // var element = document.body;
+      // element.classList.toggle("dark-mode");
+  }
+
   return (
     <div className="">
 
@@ -18,17 +27,16 @@ function App() {
         <div className="absolute w-0 h-0 hidden"><img src="./assets/Kingdom.jpeg" alt="" /></div>  {/*just for loading*/}
         <div className="relative w-max-[1080px] mt-12 mx-auto flex flex-col justify-center items-center">
 
-          <button onclick="changeIcon()" id="themeButton"
+          <button onClick={changeIcon} id="themeButton"
             className="theme-button w-[2em] h-[2em] p-1 absolute right-5 top-0 cursor-pointer flex 
               justify-start items-center
               rounded-full hover:bg-[#ffffff19] transition-all duration-200">
-            <svg className="light-mode-svg transition-all duration-500"     viewBox="0 0 24 24"><g fill="#ffff"><circle r="5" cy="12" cx="12"></circle><path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path></g></svg>
-
-            <svg className="dark-mode-svg w-5  transition-all duration-500" fill="#fff"  viewBox="0 0 384 512"><path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg>
+            <svg className="light-mode-svg transition-all duration-500" viewBox="0 0 24 24"><g fill="#ffff"><circle r="5" cy="12" cx="12"></circle><path d="m21 13h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm-17 0h-1a1 1 0 0 1 0-2h1a1 1 0 0 1 0 2zm13.66-5.66a1 1 0 0 1 -.66-.29 1 1 0 0 1 0-1.41l.71-.71a1 1 0 1 1 1.41 1.41l-.71.71a1 1 0 0 1 -.75.29zm-12.02 12.02a1 1 0 0 1 -.71-.29 1 1 0 0 1 0-1.41l.71-.66a1 1 0 0 1 1.41 1.41l-.71.71a1 1 0 0 1 -.7.24zm6.36-14.36a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm0 17a1 1 0 0 1 -1-1v-1a1 1 0 0 1 2 0v1a1 1 0 0 1 -1 1zm-5.66-14.66a1 1 0 0 1 -.7-.29l-.71-.71a1 1 0 0 1 1.41-1.41l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.29zm12.02 12.02a1 1 0 0 1 -.7-.29l-.66-.71a1 1 0 0 1 1.36-1.36l.71.71a1 1 0 0 1 0 1.41 1 1 0 0 1 -.71.24z"></path></g></svg>
+            <svg className="dark-mode-svg   w-5  transition-all duration-500" fill="#fff"  viewBox="0 0 384 512"><path d="m223.5 32c-123.5 0-223.5 100.3-223.5 224s100 224 223.5 224c60.6 0 115.5-24.2 155.8-63.4 5-4.9 6.3-12.5 3.1-18.7s-10.1-9.7-17-8.5c-9.8 1.7-19.8 2.6-30.1 2.6-96.9 0-175.5-78.8-175.5-176 0-65.8 36-123.1 89.3-153.3 6.1-3.5 9.2-10.5 7.7-17.3s-7.3-11.9-14.3-12.5c-6.3-.5-12.6-.8-19-.8z"></path></svg>
           </button>
 
           <div className="flex flex-col justify-center items-center">
-            <a href="#">
+            <a href="./">
                 {/*<img class="h-[8em] rounded-full logo" src="/assets/small c.gif" alt="">*/}
               <video loop autoplay muted class="h-[7.5em] rounded-full">
                 <source src={logo} type="video/webm"/>
